e4009dee92209ba1b829b56c31de30cc7ab9230e
Jesper Lund <jeslu246@student.liu.se>
2019-08-12 15:18:05 +0200

    update stars animation

--- js/stars.js
+++ js/stars.js
@@ -4,10 +4,10 @@ var canvas,
 	context,
 	particles,
 	particleColor = "rgb(220, 220, 220, 0.8)",
-	particleDensity = 200,
+	particleDensity = 1000,
 	motionBlur = true, //Motion blur effect on or off
-	particleSize = 0.6,
-	collisionDetection = true;//collision effect on or off, this may degrade performance after set to true
+	particleSize = 0.5,
+	collisionDetection = false;//collision effect on or off, this may degrade performance after set to true
 
 function initStars() {
 	canvas = document.getElementById('header-canvas');
@@ -56,10 +56,16 @@ function moveParticle() {
 	requestAnimationFrame(moveParticle);
 }
 
+function randomIntFromInterval(min, max) { // min and max included 
+	return Math.random() * (max - min + 1) + min;
+  }
+
 function particle() {
+	const rand = randomIntFromInterval(0.5, 1.3);
 	this.posX = Math.floor((Math.random() * window.innerWidth) + 1); //Current X position of a particle
 	this.posY = Math.floor((Math.random() * window.innerHeight) + 1); //Current Y position of a particle
-	this.speed = 0.3; //speed of particle
+	this.speed = 0.2 * rand; 
+	this.particleSize = particleSize * rand;
 	this.velocityX = (Math.random() - 0.5) * this.speed; //x Direction
 	this.velocityY = (Math.random() - 0.5) * this.speed; //Y direction
 	this.color = particleColor;
@@ -67,30 +73,31 @@ function particle() {
 	this.draw = function() {
 		context.beginPath();
 		context.fillStyle = this.color;
-		context.arc(this.posX, this.posY, particleSize, Math.PI * 4, false);
+		context.arc(this.posX, this.posY, this.particleSize, Math.PI * 4, false);
 		context.fill();
 	}
 
 	this.move = function() {
 		this.posX = (this.posX + this.velocityX);
 		this.posY = (this.posY + this.velocityY);
-
+		/*
 		//if particle reached to max X
-		if (this.posX >= (window.innerWidth - particleSize)) {
+		if (this.posX >= (window.innerWidth - this.particleSize)) {
 			this.velocityX *= -1;
 		}
 		//if particle reached to max y
-		else if (this.posY >= (window.innerHeight - particleSize)) {
+		else if (this.posY >= (window.innerHeight - this.particleSize)) {
 			this.velocityY *= -1;
 		}
 		//if particle reached to min x
-		else if (this.posX <= particleSize) {
+		else if (this.posX <= this.particleSize) {
 			this.velocityX *= -1;
 		}
 		//if particle reached to min y
-		else if (this.posY <= particleSize) {
+		else if (this.posY <= this.particleSize) {
 			this.velocityY *= -1;
 		}
+		*/
 
 		this.draw();
 	}
@@ -110,7 +117,7 @@ function particle() {
 
 		var distance = this.findDistance(particle1);
 
-		if ((distance <= 2 * particleSize)) {
+		if ((distance <= 2 * this.particleSize)) {
 			var x = this.velocityX;
 			this.velocityX = particle1.velocityX;
 			particle1.velocityX = x;
